937918594aa71693243597c5e966d8178844f7ac
Kali-Fang <kfang@wpi.edu>
2026-03-29 18:40:11 -0400

    email change

--- src/App.tsx
+++ src/App.tsx
@@ -59,11 +59,11 @@ function ContactBar() {
           </svg>
           linkedin.com/in/kali-fang
         </a>
-        <a href="mailto:kalifang1935@gmail.com" className="contact-bar__email">
+        <a href="mailto:kalifang12@gmail.com" className="contact-bar__email">
           <svg className="contact-bar__icon" viewBox="0 0 24 24" fill="currentColor">
             <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
           </svg>
-          kalifang1935@gmail.com
+          kalifang12@gmail.com
         </a>
       </div>
     </footer>
